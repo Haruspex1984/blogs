b70237b840014f387eef19d4c11979e7e5a03f44
Benjamin Renard <b.renard84@gmail.com>
2023-10-20 17:53:21 +0200

    On démarre !

---
+++ src/test/java/fr/brenard/blogs/BlogsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.brenard.blogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
